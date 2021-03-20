fb29f56762b525a9a472aa7a825f80c24b73285d
mrserg99 <58822394+mrserg99@users.noreply.github.com>
2021-03-20 18:41:18 +0300

    Delete Lab_3_war_exploded directory

--- Lab_3_war_exploded/resources/js/Time.js
+++
@@ -1,6 +0,0 @@
-function time(){
-    document.getElementById('form:time').innerText = new Date().toLocaleTimeString();
-    setInterval(function() {
-        document.getElementById('form:time').innerText = new Date().toLocaleTimeString();
-    }, 7*1000);
-}
